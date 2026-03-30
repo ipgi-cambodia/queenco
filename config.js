e1e9b143d8286f4db40c9d318f3fcefd84411375
ipgi-cambodia <timothypaulevangelista1@gmail.com>
2026-03-30 11:38:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,2 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://vypccrybontwlzqmhvys.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5cGNjcnlib250d2x6cW1odnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MTUzODUsImV4cCI6MjA5MDM5MTM4NX0.kqyCEBxEdfxyWFIyYyMfLRPyfjlMCCt-ZEXWUGf61gU",
-  supabaseTable: "landing_payload",
-  supabaseRowId: 1,
-  fallbackJsonUrl: "jackpot.json",
-  pollMsActive: 3000,
-  pollMsHidden: 15000
-};
+  window.SUPABASE_URL = "https://vypccrybontwlzqmhvys.supabase.co",
+  window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5cGNjcnlib250d2x6cW1odnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MTUzODUsImV4cCI6MjA5MDM5MTM4NX0.kqyCEBxEdfxyWFIyYyMfLRPyfjlMCCt-ZEXWUGf61gU",
