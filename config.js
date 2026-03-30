714f0d22fe410368011379ce5ec66cf93aad6a7d
ipgi-cambodia <timothypaulevangelista1@gmail.com>
2026-03-30 12:35:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-  window.SUPABASE_URL = "https://vypccrybontwlzqmhvys.supabase.co",
+  window.SUPABASE_URL = "https://vypccrybontwlzqmhvys.supabase.co";
   window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5cGNjcnlib250d2x6cW1odnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MTUzODUsImV4cCI6MjA5MDM5MTM4NX0.kqyCEBxEdfxyWFIyYyMfLRPyfjlMCCt-ZEXWUGf61gU",
